b8faf346e3156f9eb5115cf52a6c69152000be9a
codeforhim <saharshsri2004@gmail.com>
2026-01-16 14:08:38 +0530

    docker-setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,6 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 
 const app = express()
@@ -8,8 +10,8 @@ app.use(cors({
     credentials: true
 })) //to accept cookies from frontend
 
-app.use(express.json({limit: "16kb"})) //to parse json data from request body
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" })) //to parse json data from request body
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public")) //to serve static files from the public directory
 app.use(cookieParser()) //to parse cookies from request headers
 
